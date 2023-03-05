c15796d782a35c1b63ca40539a31424b26266dc5
shinayaka <18227117+shinayaka@users.noreply.github.com>
2023-03-05 21:34:00 +0900

    Markdownで記事を投稿できるようにする

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -1,12 +1,19 @@
 <template>
   <div>{{ article.title }}</div>
-  <div v-html="article.content" />
+  <div v-html="content" />
 </template>
 <script setup lang="ts">
+import { marked } from "marked";
 import { createRequest, getApiUrl } from "../../js/repository";
 const route = useRoute();
 const { data: article } = await useFetch(
   getApiUrl("articles/" + route.params.id),
   createRequest()
 );
+const content = ref();
+onMounted(() => {
+  content.value = article.value.content
+    ? marked.parse(article.value.content)
+    : "";
+});
 </script>
